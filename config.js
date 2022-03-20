3274c4f615010810c0e1814333b9fbdba2350f1b
NEYFOX <92888341+NEYFOXX@users.noreply.github.com>
2022-03-20 13:18:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
         password: 'clashofroyalefd123'
     },
     bot: {
-        token: 'OTU0ODE1MzM4NTg1OTQ4MjMx.YjYnFA.ScCS4omTOLZRwrdWt4-qtfLL_2Y',
+        token: 'Give your token',
         prefix: '+'
     }
 }
